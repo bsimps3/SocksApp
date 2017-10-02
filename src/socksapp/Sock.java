e8a4a02c6de7cfcf3bc27cf19268ccfc738832d1
bsimps3 <bsimps3@gmail.com>
2017-10-01 21:55:58 -0500

    Changed to ArrayLists
    Changed all arrays to ArrayLists for simplicity.

--- src/socksapp/Sock.java
+++ src/socksapp/Sock.java
@@ -1,11 +1,13 @@
 
+// a Sock object gets its "number" when it is created.  The number is passed to the Sock object 
+//as the integer of the constructor on line 10 below.
 package socksapp;
 
 
 public class Sock {
     private int pairNum;
     
-    public Sock (int num){  
+    public Sock (int num){   //when a Sock is created, an integer is used as the argument of Sock()
         pairNum = num;
     }
     
@@ -13,8 +15,6 @@ public class Sock {
         return pairNum;
     }
     
-    public void setNum(int num){
-        pairNum = num;
-    }
+  
     
 }
--- src/socksapp/SocksApp.java
+++ src/socksapp/SocksApp.java
@@ -1,30 +1,41 @@
 /*
 
 build/run this project to see how many times the socks will match both 
-on Wed AND on Fri out of a set number of weeks (numTrials)
+on Wed AND on Fri out of a set number of weeks (numWeeks)
 
 Mr. D said the probability should be 1/63, I think this "brute force" method 
 agrees with 1/63.
 
-*/
+ */
 package socksapp;
+
 import java.lang.Math.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SocksApp {
 
     public static void main(String[] args) {
-        
-       int numMatches = 0;
-       int numTrials = 1000;
-       for(int i = 1; i<numTrials +1 ; i++){
-          
-          Drawer one = new Drawer();
-          if(one.getSocksMatchState()){
-              numMatches++;
-          }
-       }
-       System.out.println(numMatches);
+
+        int numWeeks = 1_000_000;
+        final int WED1 = 4;
+        final int WED2 = 5;
+        final int FRI1 = 8;
+        final int FRI2 = 9;
+        int numMatches = 0;
+
+        for (int i = 1; i < numWeeks + 1; i++) {
+            Drawer ob = new Drawer();
+            int sock1 = ob.getSock(WED1).getNum();
+            int sock2 = ob.getSock(WED2).getNum();
+            int sock3 = ob.getSock(FRI1).getNum();
+            int sock4 = ob.getSock(FRI2).getNum();
+            if (sock1 == sock2 && sock3 == sock4) {
+                numMatches++;
+            }
+
+        }
+
+        System.out.println(numMatches);
+
     }
-    
 }
